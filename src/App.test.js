5b12af1dab5339f7b6725d687d773be7c6c66188
warshaya <4302702+warshaya@users.noreply.github.com>
2018-10-05 13:44:20 -0700

    add a Home component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { Home } from './home'
+
+configure({adapter: new Adapter()})
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it('contains a child Home', () => {
+  const wrapper = shallow(<App />)
+  console.log(wrapper.find(Home))
+})
---
+++ src/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Home extends React.Component {
+  render() {
+    return (<h1>Welcome to Aaron's website!</h1>)
+  }
+}
+
+export { Home }
+
